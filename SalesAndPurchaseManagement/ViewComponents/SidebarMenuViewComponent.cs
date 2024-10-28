054f5197a876880c24f7b1751ab75fa2e4982d0f
dan <minhdan190904@gmail.com>
2024-10-28 09:46:01 +0700

    fix ahhhh

--- SalesAndPurchaseManagement/ViewComponents/SidebarMenuViewComponent.cs
+++ SalesAndPurchaseManagement/ViewComponents/SidebarMenuViewComponent.cs
@@ -20,16 +20,16 @@ public class SidebarMenuViewComponent : ViewComponent
                 Icon = "fa fa-box",
                 SubItems = new List<MenuItem>
                 {
-                    new MenuItem() { Id = 17, Name = "Chi tiết sản phẩm", Link = "/Product/Index", Icon = "fas fa-box" },
-                    new MenuItem() { Id = 5, Name = "Danh mục", Link = "/Category/Index", Icon = "fas fa-list" },
-                    new MenuItem() { Id = 6, Name = "Xuất xứ", Link = "/CountryOfOrigin/Index", Icon = "fas fa-globe" },
-                    new MenuItem() { Id = 7, Name = "Đặc điểm", Link = "/Characteristic/Index", Icon = "fas fa-cogs" },
-                    new MenuItem() { Id = 8, Name = "Màu sắc", Link = "/Color/Index", Icon = "fas fa-palette" },
-                    new MenuItem() { Id = 8, Name = "Vật liệu", Link = "/Material/Index", Icon = "fas fa-cube" },
-                    new MenuItem() { Id = 10, Name = "Tính năng", Link = "/Feature/Index", Icon = "fas fa-tools" },
-                    new MenuItem() { Id = 11, Name = "Kích thước", Link = "/Size/Index", Icon = "fas fa-ruler" },
-                    new MenuItem() { Id = 12, Name = "Hình dạng", Link = "/Shape/Index", Icon = "fas fa-shapes" },
-                    new MenuItem() { Id = 13, Name = "Nhà sản xuất", Link = "/Manufacturer/Index", Icon = "fas fa-industry" }
+                    new MenuItem() { Id = 17, Name = "Chi tiết sản phẩm", Link = "/Product/Index" },
+                    new MenuItem() { Id = 5, Name = "Danh mục", Link = "/Category/Index" },
+                    new MenuItem() { Id = 6, Name = "Xuất xứ", Link = "/CountryOfOrigin/Index" },
+                    new MenuItem() { Id = 7, Name = "Đặc điểm", Link = "/Characteristic/Index" },
+                    new MenuItem() { Id = 8, Name = "Màu sắc", Link = "/Color/Index" },
+                    new MenuItem() { Id = 9, Name = "Vật liệu", Link = "/Material/Index" },
+                    new MenuItem() { Id = 10, Name = "Tính năng", Link = "/Feature/Index" },
+                    new MenuItem() { Id = 11, Name = "Kích thước", Link = "/Size/Index" },
+                    new MenuItem() { Id = 12, Name = "Hình dạng", Link = "/Shape/Index" },
+                    new MenuItem() { Id = 13, Name = "Nhà sản xuất", Link = "/Manufacturer/Index" }
                 }
             },
             new MenuItem() { Id = 14, Name = "Khách hàng", Link = "/Customer/Index", Icon = "fas fa-users" },
